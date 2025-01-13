75c46000768394703f06c116fbc4f29354d28aeb
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-13 15:29:02 +0000

    add gunicorn and amend settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r1rwow*v3biszc4&#1h+ah#-h$=_q7k=f7$bz#z+8p*v1x_snw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
