faf210be466dc0bf834584c2c4f44bc0cf8f2429
Charles Tack <gfaxxlos@students.codeinstitute.net>
2025-01-13 16:39:09 +0000

    fix local host

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r1rwow*v3biszc4&#1h+ah#-h$=_q7k=f7$bz#z+8p*v1x_snw'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
 
 # Application definition
